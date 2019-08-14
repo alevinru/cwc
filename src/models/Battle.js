489dd72ab58702e11f41814fe63590012e422842
Alexander Levin <alevin.ru@gmail.com>
2019-08-14 11:22:50 +0300

    ruBattle parser

---
+++ src/models/Battle.js
@@ -0,0 +1,43 @@
+import { Schema, model } from 'mongoose';
+
+const resultsSchema = new Schema({
+  _id: false,
+  castle: String,
+  name: String,
+  code: String,
+  gold: Number,
+  stock: Number,
+  result: String,
+  ga: Boolean,
+  difficulty: Number,
+  score: Number,
+
+  atkLeaders: [String],
+  defLeaders: [String],
+
+  atk: Number,
+  masterReport: {
+    atk: Number,
+    def: Number,
+    gold: Number,
+    id: Schema.Types.ObjectId,
+  },
+});
+
+const battle = {
+
+  date: Date,
+  reportDate: Date,
+
+  results: [resultsSchema],
+  result: Object,
+
+  text: String,
+  userId: Number,
+  ts: Date,
+
+};
+
+const schema = new Schema(battle, { collection: 'Battle' });
+
+export default model('Battle', schema);

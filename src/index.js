da3e96d4796affc5eacb3090c3f62dea0fd5d54b
Alexander Levin <alevin.ru@gmail.com>
2019-08-15 14:00:23 +0300

    separate listener

--- src/index.js
+++ src/index.js
@@ -2,41 +2,18 @@ import log from 'sistemium-telegram/services/log';
 
 import emitter, { GOT_CODE } from './api';
 import CWCTdc from './lib/CWCTdc';
-import { writeFile } from './lib/fs';
-import { battleText } from './parsers/ruBattle';
+import listener from './tdc/listener';
 
 const { debug, error } = log('index');
-
-const chats = [
-  // -1001108112459, // CW2
-  -1001369273162, // CW3
-];
-
-const tdc = new CWCTdc({ emitter, codeEvent: GOT_CODE });
-
-tdc.getClient()
-  .on('update', async update => {
-    const { chat_id: chatId } = update;
-    if (!chats.includes(chatId)) {
-      return;
-    }
-    const text = battleText(update);
-    if (!text) {
-      return;
-    }
-    const { last_message: { date } } = update;
-    debug('battle:', date, text.length);
-    try {
-      await writeFile(`./logs/battles/${date}.txt`, text);
-    } catch (e) {
-      error(e);
-      process.exit();
-    }
-  });
 
 main().catch(error);
 
 async function main() {
+
+  const tdc = new CWCTdc({ emitter, codeEvent: GOT_CODE });
+
+  tdc.getClient()
+    .on('update', listener);
 
   emitter.on(GOT_CODE, code => debug('got code', code));
 
---
+++ src/tdc/listener.js
@@ -0,0 +1,36 @@
+import log from 'sistemium-telegram/services/log';
+import { battleText } from '../parsers/ruBattle';
+import { writeFile } from '../lib/fs';
+
+const { debug, error } = log('listener');
+
+const chats = [
+  // -1001108112459, // CW2
+  -1001369273162, // CW3
+];
+
+export default async function (update) {
+
+  const { chat_id: chatId } = update;
+
+  if (!chats.includes(chatId)) {
+    return;
+  }
+
+  const text = battleText(update);
+
+  if (!text) {
+    return;
+  }
+
+  const { last_message: { date } } = update;
+  debug('battle:', date, text.length);
+
+  try {
+    await writeFile(`./logs/battles/${date}.txt`, text);
+  } catch (e) {
+    error(e);
+    process.exit();
+  }
+
+}
